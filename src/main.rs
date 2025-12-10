255829acdf5028d75fefae11cacdfb4c21604c7e
andrei399 <andreicristiantudor@gmail.com>
2025-12-10 16:21:16 +0200

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use tokio::net::{TcpStream};
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt}; 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about = "A simplified redis-like client.", long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Get {
+        key: String,
+    },
+    
+    Set {
+        key: String,
+        value: String,
+    },
+}
+
+async fn write_to_redis(mut client: TcpStream, message: &[u8]) -> io::Result<String> {
+    client.write_all(message).await?;
+    let mut buffer = [0u8; 1024];
+    let n = client.read(&mut buffer).await?;
+
+    let response = String::from_utf8_lossy(&buffer[..n]).into_owned();
+
+    if response.is_empty() {
+        return Err(io::Error::new(io::ErrorKind::InvalidData, "Redis server response is empty."))
+    }
+
+    match response.chars().next() {
+        Some('+') => {
+            let payload = &response[1..];
+            Ok(payload.trim().to_string())
+        },
+        Some('-') => {
+            let payload = &response[1..];
+            eprintln!("Error {}", payload.trim());
+            return Err(io::Error::new(io::ErrorKind::InvalidData, "Redis server returned an error"))
+        }
+        _ => {
+            eprintln!("Unexpected response format: {}", response.trim());
+            return Err(io::Error::new(io::ErrorKind::InvalidData, "Unexpected response format"))
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let cli = Args::parse();
+    let client = TcpStream::connect("127.0.0.1:6969").await?;
+
+    match cli.command {
+        Commands::Get { key } => {
+            let message = format!("GET {key}");
+            if let Some(response) = write_to_redis(client, message.as_bytes()).await.ok() {
+                println!("{}", response);
+            }
+        }
+        Commands::Set { key, value } => {
+            let message = format!("SET {key} {value}");
+            if let Some(response) = write_to_redis(client, message.as_bytes()).await.ok() {
+                println!("{}", response);
+            }
+        }
+    }
+    Ok(())
+}
+
